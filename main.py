13f6b769fb3eb789688a138bcf98ec4e476d00e5
wlonestar <wlonestar@163.com>
2026-07-28 14:46:52 +0800

    pass 10

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import sys
 store = {}
+expiry = {}  # key -> absolute ms timestamp
+clock = 0    # simulated clock in ms
+
 def eb(s):
     if s is None: return "$-1\r\n"
     return f"${len(s)}\r\n{s}\r\n"
@@ -7,60 +10,47 @@ def es(s): return f"+{s}\r\n"
 def ee(m): return f"-{m}\r\n"
 def ei(n): return f":{n}\r\n"
 
-def parse_value(key):
-    value = 0
-    if key in store:
-        value_str = store[key]
-        try:
-            value = int(value_str)
-        except (ValueError, TypeError):
-            return None
-    return value
+def check_expiry(key):
+    if key in expiry and clock >= expiry[key]:
+        del store[key]
+        del expiry[key]
 
 def handle(args):
+    global clock
     cmd = args[0].upper()
-    if cmd == "PING": return es("PONG") if len(args)==1 else eb(args[1])
-    elif cmd == "ECHO": return eb(args[1])
-    elif cmd == "SET":
-        key, val = args[1], args[2]
-        flags = [a.upper() for a in args[3:]]
-        if "NX" in flags and key in store: return "$-1\r\n"
-        if "XX" in flags and key not in store: return "$-1\r\n"
-        store[key] = val; return es("OK")
-    elif cmd == "GET": return eb(store.get(args[1]))
-    elif cmd == "DBSIZE": return ei(len(store))
-    elif cmd == "INCR":
-        # Get current value (default "0"), parse as int, add 1, store, return new value
-        value = parse_value(args[1])
-        if value is None:
-            return ee("ERR value is not an integer or out of range")
-        value += 1
-        store[args[1]] = value
-        return ei(value)
-    elif cmd == "DECR":
-        # Same as INCR but subtract 1
-        value = parse_value(args[1])
-        if value is None:
-            return ee("ERR value is not an integer or out of range")
-        value -= 1
-        store[args[1]] = value
-        return ei(value)
-    elif cmd == "INCRBY":
-        # Increment by args[2]
-        value = parse_value(args[1])
-        if value is None:
-            return ee("ERR value is not an integer or out of range")
-        value += int(args[2])
-        store[args[1]] = value
-        return ei(value)
-    elif cmd == "DECRBY":
-        # Decrement by args[2]
-        value = parse_value(args[1])
-        if value is None:
-            return ee("ERR value is not an integer or out of range")
-        value -= int(args[2])
-        store[args[1]] = value
-        return ei(value)
+    if cmd == "WAIT":
+        clock += int(args[1]); return es("OK")
+    if cmd == "SET":
+        store[args[1]] = args[2]; return es("OK")
+    elif cmd == "GET":
+        check_expiry(args[1])
+        return eb(store.get(args[1]))
+    elif cmd == "EXPIRE":
+        # Set expiry[args[1]] = clock + int(args[2]) * 1000
+        expiry[args[1]] = clock + int(args[2]) * 1000
+        # Return :1 if key exists, :0 if not
+        return ei(1 if args[1] in store else 0)
+    elif cmd == "TTL":
+        # Return :-2 if key missing, :-1 if no expiry, else remaining seconds
+        val: int = 0
+        if args[1] not in store:
+            val = -2
+        else:
+            if args[1] not in expiry:
+                val = -1
+            else:
+                val = int(int(expiry[args[1]]) / 1000)
+        return ei(val)
+    elif cmd == "PERSIST":
+        # Remove expiry, return :1 if had expiry, :0 if not
+        if args[1] in expiry:
+            expiry.pop(args[1])
+            return ei(1)
+        return ei(0)
+    elif cmd == "PING": return es("PONG") if len(args)==1 else eb(args[1])
+    elif cmd == "DBSIZE":
+        # TODO: Count only non-expired keys
+        pass
     return ee(f"ERR unknown command '{cmd}'")
 
 def pa(line):
